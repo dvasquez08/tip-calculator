e111cc569d0d34934f4c1fe3f63f9fc9d90ebf04
dvasquez08 <dvasquez.422@gmail.com>
2024-02-08 23:42:01 -0700

    Styled the app

---
+++ lib/TipCalculatorScreen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'components.dart';
+
+class TipCalculatorScreen extends StatefulWidget {
+  const TipCalculatorScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TipCalculatorScreen> createState() => _TipCalculatorScreenState();
+}
+
+class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
+  double billAmount = 0.0;
+  double tipPercentage = 0.1;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: const SansText('Tip Calculator', 25.0),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/bg.jpg'), fit: BoxFit.cover)),
+          child: ListView(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(0, 0, 0, 0.5),
+                    borderRadius: BorderRadius.circular(20.0)),
+                alignment: Alignment.center,
+                padding: const EdgeInsets.only(
+                    left: 10.0, right: 10.0, top: 8.0, bottom: 8.0),
+                margin:
+                    const EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    TextField(
+                      keyboardType: TextInputType.number,
+                      onChanged: (value) {
+                        setState(() {
+                          billAmount = double.parse(value);
+                        });
+                      },
+                      decoration: const InputDecoration(
+                        labelText: 'Enter bill amount',
+                        labelStyle: TextStyle(color: Colors.white),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                              color: Colors
+                                  .white), // Set the color of the underline
+                        ),
+                      ),
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(height: 20.0),
+                    const SansText('Select Tip Percentage', 20.0),
+                    Slider(
+                      value: tipPercentage,
+                      onChanged: (value) {
+                        setState(() {
+                          tipPercentage = value;
+                        });
+                      },
+                      min: 0.1,
+                      max: 0.3,
+                      divisions: 4,
+                      label: '${(tipPercentage * 100).round()}%',
+                    ),
+                    const SizedBox(height: 20.0),
+                    SansText(
+                        'Tip Amount: \$${(billAmount * tipPercentage).toStringAsFixed(2)}',
+                        20.0),
+                    const SizedBox(height: 10),
+                    SansText(
+                        'Total Amount: \$${(billAmount + (billAmount * tipPercentage)).toStringAsFixed(2)}',
+                        20.0),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,77 @@
 import 'package:flutter/material.dart';
-import 'components.dart';
+import 'TipCalculatorScreen.dart';
 
 void main() {
-  runApp(TipCalculator());
+  runApp(MyApp());
 }
 
-class TipCalculator extends StatelessWidget {
-  const TipCalculator({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tip Calculator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: TipCalculatorScreen(),
     );
   }
 }
 
-class TipCalculatorScreen extends StatefulWidget {
-  const TipCalculatorScreen({Key? key}) : super(key: key);
-
-  @override
-  State<TipCalculatorScreen> createState() => _TipCalculatorScreenState();
-}
-
-class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
-  double billAmount = 0.0;
-  double tipPercentage = 0.1;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const SansText('Tip Calculator', 25.0),
-        ),
-        body: Container(
-          child: ListView(
-            children: [
-              TextField(
-                keyboardType: TextInputType.number,
-                onChanged: (value) {
-                  setState(() {
-                    billAmount = double.parse(value);
-                  });
-                },
-                decoration: InputDecoration(labelText: 'Enter bill amount'),
-              ),
-              SizedBox(height: 20.0),
-              SansText('Select Tip Percentage', 20.0),
-              Slider(value: tipPercentage, onChanged: (value) {
-                setState(() {
-                  tipPercentage = value;
-                });
-              },
-              min: 1,
-              max: 0.3,
-              divisions: 4,
-                label: '${(tipPercentage * 100).round()}%',
-              ),
-              SizedBox(height: 20.0),
-              SansText('Tip Amount: \$${(billAmount * tipPercentage).toStringAsFixed(2)}', 20.0),
-              SizedBox(height: 10.0),
-              SansText('Total Amount: \$${(billAmount + (billAmount * tipPercentage)).toStringAsFixed(2)}', 20.0),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class TipCalculatorScreen extends StatefulWidget {
+//   const TipCalculatorScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   State<TipCalculatorScreen> createState() => _TipCalculatorScreenState();
+// }
+//
+// class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
+//   double billAmount = 0.0;
+//   double tipPercentage = 0.1;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const SansText('Tip Calculator', 25.0),
+//         ),
+//         body: Container(
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               TextField(
+//                 keyboardType: TextInputType.number,
+//                 onChanged: (value) {
+//                   setState(() {
+//                     billAmount = double.parse(value);
+//                   });
+//                 },
+//                 decoration: InputDecoration(labelText: 'Enter bill amount'),
+//               ),
+//               SizedBox(height: 20.0),
+//               SansText('Select Tip Percentage', 20.0),
+//               Slider(value: tipPercentage, onChanged: (value) {
+//                 setState(() {
+//                   tipPercentage = value;
+//                 });
+//               },
+//               min: 0.1,
+//               max: 0.3,
+//               divisions: 4,
+//                 label: '${(tipPercentage * 100).round()}%',
+//               ),
+//               SizedBox(height: 20.0),
+//               Text('Tip Amount: \$${(billAmount * tipPercentage).toStringAsFixed(2)}'),
+//               SizedBox(height: 10),
+//               Text('Total Amount: \$${(billAmount + (billAmount * tipPercentage)).toStringAsFixed(2)}'),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 
