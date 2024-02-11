0c7ed9b3a0d550cfeebc3dfb578f1a17601e13f6
dvasquez08 <dvasquez.422@gmail.com>
2024-02-11 03:23:32 -0700

    Fixed per person bug, added comments

--- lib/TipCalculatorScreen.dart
+++ lib/TipCalculatorScreen.dart
@@ -27,6 +27,9 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
           centerTitle: true,
           title: const SansText('Tip Calculator', 30.0),
         ),
+
+        // Beginning of app content. This container adds the background image to the screen
+
         body: Container(
           decoration: const BoxDecoration(
               image: DecorationImage(
@@ -34,6 +37,9 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
           child: ListView(
             children: [
               const SizedBox(height: 40.0),
+
+              // ===== Beginning of main tip calculator =====
+
               Container(
                 decoration: BoxDecoration(
                     color: const Color.fromRGBO(0, 0, 0, 0.5),
@@ -47,6 +53,9 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
+
+                    // ===== Section where user enters the bill amount =====
+
                     TextField(
                       keyboardType: TextInputType.number,
                       onChanged: (value) {
@@ -65,6 +74,11 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
                       ),
                       style: const TextStyle(color: Colors.white, fontSize: 25.0),
                     ),
+
+                    // ===== End of user bill amount section =====
+
+                    // ==== Beginning of tip percentage slider =====
+
                     const SizedBox(height: 20.0),
                     const SansText('Select Tip Percentage', 25.0),
                     Slider(
@@ -77,14 +91,21 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
                       min: 0.1,
                       max: 0.3,
                       divisions: 4,
-                      // label: '${(tipPercentage * 100).round()}%',
                     ),
+
+                    // ===== Displaying the tip percentage =====
+
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         SansText('${(tipPercentage * 100).round()}%', 20.0)
                       ],
                     ),
+
+                    // ===== End of tip percentage selection slider =====
+
+                    // ===== Calculated amounts after user interaction =====
+
                     const SizedBox(height: 20.0),
                     SansText(
                         'Tip Amount: \$${(billAmount * tipPercentage).toStringAsFixed(2)}',
@@ -97,10 +118,20 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
                 ),
               ),
               const SizedBox(height: 10.0),
+
+              // ===== End of main tip calculator =====
+
+              // ===== Column for additional calculations such as amount of people or when wanting to round the bill =====
+
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+
+                  // ===== Beginning of bill per person calculator ======
+
+                  // This part takes the amount of people and splits the bill accordingly
+
                   Container(
                     decoration: BoxDecoration(
                         color: const Color.fromRGBO(0, 0, 0, 0.5),
@@ -139,10 +170,17 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
                           ],
                         ),
                         const SizedBox(height: 10.0),
-                        SansText('Amount per person: \$${((billAmount + (billAmount * tipPercentage)) / numberOfPeople )}',  23.0),
+                        SansText('Amount per person: \$${((billAmount + (billAmount * tipPercentage)) / numberOfPeople).toStringAsFixed(2)}',  23.0),
                       ],
                     ),
                   ),
+
+                  // ===== End of per person calculator section =====
+
+                  // ===== Beginning of rounding section =====
+
+                  // This part rounds  the bill to the nearest dollar, up or down depending on what the user decides
+
                   Container(
                     decoration: BoxDecoration(
                         color: const Color.fromRGBO(0, 0, 0, 0.5),
@@ -158,6 +196,9 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
+
+                            // ===== Rounding the bill up =====
+
                             const SansText('Round Up', 20.0),
                             IconButton(
                               icon: const Icon(Icons.arrow_downward),
@@ -169,6 +210,8 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
                                 });
                               },
                             ),
+
+                            // ===== Rounding the bill down =====
 
                             const SansText('Round Down', 20.0),
                             IconButton(
@@ -183,19 +226,29 @@ class _TipCalculatorScreenState extends State<TipCalculatorScreen> {
                             ),
                           ],
                         ),
+
+                        // ===== Showing the rounded amount =====
                         const SizedBox(height: 10.0),
                         SansText('Rounded Amount: \$${roundTotalAmount().toStringAsFixed(0)}', 23.0),
                       ],
                     ),
                   )
+
+                  // ===== End of rounding section =====
+
                 ],
-              )// opacity container
+              )
+
+              // ===== End of additional calculations section  =====
+
             ],
           ),
         ),
       ),
     );
   }
+
+  // ===== Functions being called for rounding the bill =====
 
   double roundTotalAmount() {
     double totalAmount = billAmount + (billAmount * tipPercentage);
